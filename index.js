ad34cbad8e50aed4fadbba3037a16d613d8f9fb4
Michael Trpak <mrtrpak3@gmail.com>
2022-06-16 15:06:18 -0400

    initial

---
+++ index.js
@@ -0,0 +1,35 @@
+const axios = require('axios').default,
+ express = require('express'),
+ cors = require("cors"),
+ path = require('path'),
+ PORT = process.env.PORT || 3333,
+ app = express();
+
+ const { soccerKey } = require('./secret');
+
+app.use(cors());
+
+//serve static files from react app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/soccer',  async (req,res) => {
+  try {
+    const response = await axios({
+      "url": `https://api.football-data.org/v2/competitions/BL1/standings`,
+      "headers": {
+        "X-Auth-Token": soccerKey,
+        "Content-Type": "application/json"
+      }
+    })
+    console.log(response.data.standings[0].table);
+    res.send(response.data.standings[0].table);
+  } catch (err) {
+    res.send(err);
+  };
+});
+
+//catchall handler for any request that doesn't match above options
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+})
+app.listen(PORT, () => console.log(`server live on http://localhost:${PORT}`));
