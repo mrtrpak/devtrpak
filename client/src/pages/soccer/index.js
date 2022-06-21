f4176427e873f087eda18d0ea39e55f0eddb7528
Michael Trpak <mrtrpak3@gmail.com>
2022-06-21 11:14:10 -0400

    fixing react object error

--- client/src/pages/soccer/index.js
+++ client/src/pages/soccer/index.js
@@ -1,19 +1,27 @@
 import React, { useState, useEffect} from 'react';
 
 export default function SoccerPage() {
-  let [standings, setStandings] = useState({ table: {}});
+  let [standings, setStandings] = useState({});
 
   useEffect(() => {
     fetch('/api/soccer')
     .then(res => res.json())
-    .then(standings => setStandings({ table: standings }))
+    .then(standings => setStandings({ standings }))
     .catch(err => console.log(err));
   }, []);
+
+  console.log(standings, "log standings");
 
   return (
     <div>
       <h1>Here are soccer standings</h1>
-      { standings }
+      { standings.map(((team, id) => {
+        return (
+          <div>
+            team[id].team.name
+          </div>
+          ) 
+      })) }
     </div>
   );
 };
