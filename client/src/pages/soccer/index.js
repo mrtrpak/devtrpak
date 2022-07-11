38af30926d1cfdae512bb98e870be152ec74e041
Michael Trpak <mrtrpak3@gmail.com>
2022-07-11 14:37:17 -0400

    fixing syntax for consistency and trying out react-table-scrollbar npm

--- client/src/pages/soccer/index.js
+++ client/src/pages/soccer/index.js
@@ -7,27 +7,11 @@ import SoccerTableBody from '../../components/soccer-tableBody';
 import './index.css';
 
 export default function SoccerPage() {
-  const [isSticky, setSticky] = useState(false);
-  const ref = useRef(null);
-
-  const handleScroll = () => {
-    if (ref && ref.current) {
-      setSticky(ref.current.getBoundingClientRect().top <= 0);
-    };
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', () => handleScroll);
-    };
-  }, []);
 
   return (
-    <TableContainer component={Paper} >
-      <Table aria-label="simple table" className={`soccerPage sticky-wrapper${isSticky ? ' sticky' : ''}`} ref={ref}>
-        <SoccerTableHeaders />
+    <TableContainer component={Paper} className='soccer-tableContainer'>
+      <Table aria-label="simple table" className='soccer-table'>
+        <SoccerTableHeaders className='sticky' />
         <SoccerTableBody />
       </Table>
     </TableContainer>
