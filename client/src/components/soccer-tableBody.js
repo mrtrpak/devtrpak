2ca5aa751c2b3865bdf42061b8f694d9a6609065
Michael Trpak <mrtrpak3@gmail.com>
2022-07-12 02:15:10 -0400

    trying new way to fix deployed ability to get the data

--- client/src/components/soccer-tableBody.js
+++ client/src/components/soccer-tableBody.js
@@ -20,30 +20,32 @@ export default function SoccerTableBody() {
     fetchStandings();
   }, []);
 
-  return (
-    <TableBody className='soccer-tableBody'>
-      { !standings ? <TableRow></TableRow> :
-        Object.entries(standings).map((team, idx) => {
-          const { name, crestUrl } = team[1].team;
-          const { position, playedGames, won, draw, lost, points, goalsFor, goalsAgainst, goalDifference } = team[1];
-          
-          return (
-            <TableRow key={idx} className={`place-${position}`}>
-              <TableCell align='center'><span className='position-text'>{position}</span></TableCell>
-              <TableCell align='center'><img src={crestUrl} alt="team crest" className='image'></img></TableCell>
-              <TableCell align='center'>{name}</TableCell>
-              <TableCell align='center'><span className='points-text'>{points}</span></TableCell>
-              <TableCell align='center'>{playedGames}</TableCell>
-              <TableCell align='center'>{won}</TableCell>
-              <TableCell align='center'>{draw}</TableCell>
-              <TableCell align='center'>{lost}</TableCell>
-              <TableCell align='center'>{goalsFor}</TableCell>
-              <TableCell align='center'>{goalsAgainst}</TableCell>
-              <TableCell align='center'>{goalDifference}</TableCell>
-            </TableRow>
-          )
-        })
-      }
-    </TableBody>
-  );
+  if (standings) {
+    return (
+      <TableBody className='soccer-tableBody'>
+        { 
+          Object.entries(standings).map((team, idx) => {
+            const { name, crestUrl } = team[1].team;
+            const { position, playedGames, won, draw, lost, points, goalsFor, goalsAgainst, goalDifference } = team[1];
+            
+            return (
+              <TableRow key={idx} className={`place-${position}`}>
+                <TableCell align='center'><span className='position-text'>{position}</span></TableCell>
+                <TableCell align='center'><img src={crestUrl} alt="team crest" className='image'></img></TableCell>
+                <TableCell align='center'>{name}</TableCell>
+                <TableCell align='center'><span className='points-text'>{points}</span></TableCell>
+                <TableCell align='center'>{playedGames}</TableCell>
+                <TableCell align='center'>{won}</TableCell>
+                <TableCell align='center'>{draw}</TableCell>
+                <TableCell align='center'>{lost}</TableCell>
+                <TableCell align='center'>{goalsFor}</TableCell>
+                <TableCell align='center'>{goalsAgainst}</TableCell>
+                <TableCell align='center'>{goalDifference}</TableCell>
+              </TableRow>
+            )
+          })
+        }
+      </TableBody>
+    );
+  } else return null; 
 };
